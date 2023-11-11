83c57e3853ad9763d8b78422c4aed63fd81bfc41
Shintaro Tsuge <shin.kabu7201@gmail.com>
2023-11-11 15:07:11 +0900

    Initial Commit

---
+++ RendaArrange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RendaArrange
+//
+//  Created by 柘植紳太郎 on 2023/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
