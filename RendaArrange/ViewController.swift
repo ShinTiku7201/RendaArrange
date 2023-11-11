d2cec92f5160f19e6a4392f71197c3ca0774b916
Shintaro Tsuge <shin.kabu7201@gmail.com>
2023-11-11 19:46:54 +0900

    firstcommit

--- RendaArrange/ViewController.swift
+++ RendaArrange/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var tapCount: Int = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tap() {
+        
+        tapCount = tapCount - 1
+        
+        countLabel.text = String(tapCount)
+        
+        if tapCount == 0 {
+            countLabel.text = "われたよ！"
+            imageView.image = UIImage(named: "unicorn")
+       
+        } else if tapCount <= 30 {
+            imageView.image = UIImage(named:"egg2")
+        } else if tapCount <= 50 {
+            imageView.image = UIImage(named:"egg1")
+        } else {
+            imageView.image = UIImage(named:"egg")
+            
+            
+        }
     }
 
 
